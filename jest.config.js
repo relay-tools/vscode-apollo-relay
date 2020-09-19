1cf9bc10b160dad27819ef5afd73746e368ebf80
Eloy Durán <eloy.de.enige@gmail.com>
2020-09-19 22:14:02 +0200

    [jest] No need for coverage report

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,4 @@ module.exports = {
   moduleFileExtensions: ["ts", "js", "json"],
   testMatch: ["**/tests/**/*.ts", "**/*.test.ts"],
   transform: { "\\.ts$": "ts-jest" },
-  coverageReporters: ["lcov", "text-summary"],
-  collectCoverage: !!process.env.CI,
-  collectCoverageFrom: ["src/**/*.ts"],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
 }
